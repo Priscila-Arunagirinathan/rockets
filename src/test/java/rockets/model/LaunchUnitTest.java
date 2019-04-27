e9d0f5b0fe3c44a567f61de43a0fdab0a0c277e3
Priscila <paru0002@student.monash.edu>
2019-04-27 13:34:35 +1000

    ignoring files

--- src/test/java/rockets/model/LaunchUnitTest.java
+++ src/test/java/rockets/model/LaunchUnitTest.java
@@ -7,8 +7,6 @@ import org.junit.jupiter.api.parallel.Execution;
 import java.util.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-//test1 test1111111 wwwwnnn
-
 class LaunchUnitTest {
     private Launch target;
 
